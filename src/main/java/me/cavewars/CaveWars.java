a709462f4687a8ce8b241ce53e7d212d3befc617
podpaska <kaerolastaff@gmail.com>
2025-11-30 01:06:51 +0100

    Add files via upload

---
+++ src/main/java/me/cavewars/CaveWars.java
@@ -0,0 +1,6 @@
+package me.cavewars;
+import org.bukkit.plugin.java.JavaPlugin;
+public class CaveWars extends JavaPlugin {
+    @Override public void onEnable() { getLogger().info("CaveWars enabled"); }
+    @Override public void onDisable() { }
+}
